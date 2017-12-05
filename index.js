b377f1861c3e3b40694345b0cc94d23c4d11de3c
surekha <surekhasrao@gmail.com>
2017-12-05 18:19:43 +0000

    Geolocation and API call works

--- index.js
+++ index.js
@@ -1,18 +1,24 @@
-/*global $ APIKEY*/
-$(document).ready(function(){
-    $.ajax({
-          method: "GET",
-          url: "https://http://api.openweathermap.org/data/2.5/find?",
-          data: { lat: "business", country: "us", language: "en", apiKey:APIKEY},
-          success:function(data){
-              if(data.status === "ok"){
-                  console.log(data);
-                  for (var i=0; i < data.sources.length; i++){
-                      var source = document.createElement("OPTION");
-                      source.setAttribute("value", data.sources[i].id);
-                      source.innerHTML = data.sources[i].name;
-                      document.getElementById('selection').appendChild(source);
-                  }
-                } 
-            }
-        });
+/*global $ APIKEY navigator*/
+$(document).ready(function() {
+	var lati, long;
+	if (navigator.geolocation) {
+		navigator.geolocation.getCurrentPosition(function(position) {
+			console.log("gotlocation");
+			lati = position.coords.latitude;
+			long = position.coords.longitude;
+			$("#data").html("latitude: " + lati + "<br>longitude: " + long);
+			$.ajax({
+				method: "GET",
+				url: "https://api.openweathermap.org/data/2.5/weather",
+				data: {
+					lat: lati,
+					lon: long,
+					appid: APIKEY
+				},
+				success: function(data) {
+					console.log(data);
+				}
+			});
+		});
+	}
+});
