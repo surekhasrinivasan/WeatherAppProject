cbc14768f50b5e0827d5e25d45936b5047d4a34d
surekha <surekhasrao@gmail.com>
2017-12-07 18:44:19 +0000

    Weather App shows temperature in celsius and Fahrenheit

--- index.js
+++ index.js
@@ -6,17 +6,28 @@ $(document).ready(function() {
 			console.log("gotlocation");
 			lati = position.coords.latitude;
 			long = position.coords.longitude;
-			$("#data").html("latitude: " + lati + "<br>longitude: " + long);
+			//$("#data").html("latitude: " + lati + "<br>longitude: " + long);
 			$.ajax({
 				method: "GET",
 				url: "https://api.openweathermap.org/data/2.5/weather",
 				data: {
 					lat: lati,
 					lon: long,
+					units: "metric",
 					appid: APIKEY
 				},
 				success: function(data) {
 					console.log(data);
+					document.getElementById('location').innerHTML = data.name;
+					document.getElementById('weather').innerHTML = data.weather[0].main;
+					document.getElementById('weatherdes').innerHTML = data.weather[0].description;
+					document.getElementById('icon').src = "https://openweathermap.org/img/w/" + data.weather[0].icon + ".png";
+					document.getElementById('temp').innerHTML = data.main.temp + "&#8451";
+					$("#temp").click (function(){
+						var ftemp = data.main.temp * 1.8 + 32;
+						document.getElementById('temp').innerHTML = ftemp + "&#8457";
+					});
+					
 				}
 			});
 		});
