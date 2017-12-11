4100b2b064e16d8d8b64ed9cd02fc7ea1db59e2b
surekha <surekhasrao@gmail.com>
2017-12-11 17:35:12 +0000

    Display date, increased the image size and fixed decimal points to 1 for temperature

--- index.js
+++ index.js
@@ -16,21 +16,22 @@ $(document).ready(function() {
 				},
 				success: function(data) {
 					document.getElementById('location').innerHTML = data.name;
+					document.getElementById("dayanddate").innerHTML = Date();
 					document.getElementById('weather').innerHTML = data.weather[0].main;
 					document.getElementById('weatherdes').innerHTML = data.weather[0].description;
 					var wspeed = data.wind.speed * 2.23694;
 					document.getElementById('windspeed').innerHTML = wspeed.toFixed(2) + " mph";
 					document.getElementById('icon').src = "https://openweathermap.org/img/w/" + data.weather[0].icon + ".png";
-					document.getElementById('temp').innerHTML = data.main.temp + "&#8451";
+					var ctemp = data.main.temp;
+					document.getElementById('temp').innerHTML = ctemp.toFixed(1) + "&#8451";
 					$('#temp').click(function() {
-						var ctemp = data.main.temp;
 						var ftemp = data.main.temp * 1.8 + 32;
 						var tempType = $("#temptype").val();
 						if (tempType == "C") {
-							document.getElementById('temp').innerHTML = ftemp + "&#8457";
+							document.getElementById('temp').innerHTML = ftemp.toFixed(1) + "&#8457";
 							$("#temptype").val("F");
 						} else {
-							document.getElementById('temp').innerHTML = ctemp + "&#8451";
+							document.getElementById('temp').innerHTML = ctemp.toFixed(1) + "&#8451";
 							$("#temptype").val("C");
 						}
 					});
